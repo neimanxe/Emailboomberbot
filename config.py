fdfdcd03dd529b7f175b1ed93451610876a012e3
{🇮🇳} シヴァ <mishraxe@gmail.com>
2024-08-24 06:55:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 from os important getenv
 
-API_ID
-API_HASH
-BOT_TOKEN
-OWNER_ID
-MONGO_URI
+API_ID = int(getenv("API_ID", "28102624"))
+
+API_HASH = getenv("API_HASH", "4e03913f9a576278ed4dbcdf7073e1b0")
+
+BOT_TOKEN = getenv("BOT_TOKEN", "7332091694:AAH6lor2Z0EOIjcYiEF8OxwMdeYM2zOhk-w")
+
+OWNER_ID = int(getenv("OWNER_ID", "7422183592"))
+
+MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority")
